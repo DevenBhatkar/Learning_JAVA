bd0f1c7979583790d5c77e6d97bc042968b100cb
DevenBhatkar <98800932+DevenBhatkar@users.noreply.github.com>
2025-03-19 20:28:54 +0530

    init commit

---
+++ src/JAVA_20_Practiceset5.java
@@ -0,0 +1,132 @@
+public class JAVA_20_Practiceset5 {
+
+    public static void main(String[] args) {
+        /*
+        Q1)write a program to print the following patern
+        ****
+        ***
+        **
+        *
+
+        */
+
+//        int n=4;
+//        for(int i=n;i>0;i--){
+//            for(int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+
+//        }
+
+
+        /*Q2)Write a program to sum first n even number.
+         */
+
+//        int sum =0;
+//        int n=4;
+//        for(int i=0;i<n;i++){
+//            sum = sum +(2*i);
+//        }
+//        System.out.print(sum);
+
+        /*Q3)Write  program to print multiplication table of a given
+        number n.
+         */
+
+
+//        int n=5;
+//
+//        for(int i=1;i<=10;i++){
+//            System.out.printf("%d X %d =%d\n", n , i , n*i);
+//        }
+
+        /*Q4)Write a program to print multiplication table of 10 in
+        reverse order.
+         */
+
+//        int n=10;
+//
+//        for (int i=10;i>=0;i--){
+//            System.out.printf("%d X %d = %d\n",n,i,n*i);
+//        }
+
+        /*Q5)Write a program to find factorial of a given number using
+        for loops.
+         */
+//      what is factorial ?-- n=n*n-1 * n-2 ...1
+//        int n=3;
+//        int factorial =1;
+//
+//        for (int i=1;i<=n;i++){
+//            factorial *=i ;
+//
+//        }
+//        System.out.println(factorial);
+
+
+        /*Q6) Write a program to find factorial of a given number using
+        while loops.
+         */
+
+//        int n= 5;
+//        int fac =1;
+//        int i=1;
+//
+//        while (i<=n){
+//            fac *=i;
+//            i++;
+//        }
+//        System.out.println(fac);
+
+        /*Q8)what can be done using one type of loop can also be done using
+         the other two types of loops , True or False ?
+
+         ans-True
+         */
+
+        /*Q9)write a program to calculate the sum of the number
+        occuring in the multiplication table of 8.
+         */
+
+//        int n=8;
+//        int sum = 0;
+//
+//        for(int i=1;i<=10;i++){
+//            sum += n*i;
+//        }
+//
+//        System.out.println(sum);
+
+
+
+        /*Q10)A do while loop is executed
+              1> At least once
+              2> At least twice
+              3> At most once.
+
+              Ans- 1- At least once
+         */
+
+/*repeat ques 2 using while loop
+
+ */
+//        int n =3;
+//        int sum =0;
+//        int i =0;
+//
+//        while(i<n){
+//            sum +=(2*i);
+//            i++;
+//        }
+//
+//        System.out.println(sum);
+
+
+
+
+
+
+
+    }
+}
